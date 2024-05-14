a6b5bd8ffbf2a04c7198697eb732b192f4e78176
kcl2154 <kcl2154@columbia.edu>
2024-05-14 09:47:36 -0700

    comments added + some code changes

--- src/main/java/chompJava/model/Chomp.java
+++ src/main/java/chompJava/model/Chomp.java
@@ -27,8 +27,13 @@ public class Chomp {
   public void reset() {
     chomped = 0;
     status = ChompStatus.ONGOING;
-    //reset board size??
-    throw new UnsupportedOperationException();
+    player = 0; //this is assuming p1 always goes first
+    for(int i = 0; i< board.length;i++){ //resets the entire board to not chomped again
+      for(int j = 0; j<board.length;j++){
+        board[i][j] = 0;
+      }
+    }
+    //throw new UnsupportedOperationException();
   }
 
   public ChompStatus getStatus() {
@@ -47,7 +52,7 @@ public class Chomp {
   }
 
   public boolean isLoser() {
-    if(board[0][0]==1){
+    if(board[0][0]==1){ //checks if poison piece is eaten
       return true;
     }
     else{
@@ -58,7 +63,7 @@ public class Chomp {
 
   public String getCurrentPlayer() {
     if(player == 0){
-      return "player 1";
+      return "player 1"; //do you want to return it as "player 1" or just "p1"
     }
     else{
       return "player 2";
@@ -69,40 +74,33 @@ public class Chomp {
   public ChompStatus chomp(int row, int col) {
     validateLocation(row, col);
     int rowindex = this.rows;
-    while(rowindex>=rows){
+    while(rowindex>=row){ //while the index is still at the chomped param row and below
       if(board[rowindex][col]==0){
-        board[rowindex][col] = 1;
-        chomped++;
+        board[rowindex][col] = 1; //chomps all rows at and below "row"
+        chomped++; //updates # of pieces chomped
       }
-      rowindex--;
+      rowindex--; //updates rowindex
     }
     int colindex = this.cols;
-    while(colindex>=cols){
+    while(colindex>=col){ //while the index is still at the chomped column or to the right
       if(board[row][colindex]==0){
-        board[row][colindex]=1;
-        chomped++;
+        board[row][colindex]=1; //chomps all rows to the right of "col"
+        chomped++; //updates chomped
       }
-      colindex--;
+      colindex--; //updates colindex
     }
-    return getStatus();
+    return getStatus(); //automatically will check from here if the poison piece is eaten
   }
 
   @Override
   public String toString() {
-    String currentplayer="";
-    if(player ==0){
-      currentplayer = "p1";
-    }
-    else{
-      currentplayer = "p2";
-    }
-    return "board: " + board + ", current player: " + currentplayer + ", game status: " + status;
+    return "board: " + board + ", current player: " + getCurrentPlayer() + ", game status: " + getStatus();
     //throw new UnsupportedOperationException();
   }
 
   private void validateLocation(int row, int col) {
-    //also if the row and col fall out of the 2d grid
-    if(row==0&&col==0){
+    //if the row and col fall out of the 2d grid
+    if(row>this.rows||col>this.cols){
       throw new UnsupportedOperationException();
   }
   }
